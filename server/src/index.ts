3dcf6356f16a9c898e3410d0ee8cfe4c60eaab84
Matthew Frankland <admin@matthewfrankland.co.uk>
2025-07-17 21:40:47 +0100

    Run Updates, Add Missing Flex Wrap

--- server/src/index.ts
+++ server/src/index.ts
@@ -95,7 +95,7 @@ app.use ( helmet ( {
 
 app.use ( "/assets", rateLimit ( {
   windowMs: 60 * 60 * 1000, // 1 hour
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 1000, // limit each IP to 100 requests per windowMs
   message: "Too many requests, please try again later.",
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
--- server/src/routes/static.ts
+++ server/src/routes/static.ts
@@ -13,7 +13,7 @@ export const router = Router ( )
 
 router.use ( rateLimit ( {
   windowMs: 60 * 1000, // 1 minute
-  max: 500, // limit each IP to 500 requests per windowMs
+  max: 1000, // limit each IP to 500 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: {
