b1056647e60714ffd1dbe718a555c338154b7751
Matthew Frankland <admin@matthewfrankland.co.uk>
2025-08-29 08:09:09 +0100

    Updates dependencies and Dockerfile
    Updates Angular and other dependencies in `package.json` and `package-lock.json` to their latest versions.
    
    Refactors the Dockerfile: creates non-root user and fixes Dockerfile for deployment.
    
    Adds a check for CR_PAT in `deploy.sh` and provides an example.
    
    Updates rate limit configuration.

--- server/src/routes/drive.ts
+++ server/src/routes/drive.ts
@@ -12,7 +12,7 @@ export const router = Router ( )
 
 router.use ( rateLimit ( {
   windowMs: 60 * 1000, // 1 minute
-  max: 10, // limit each IP to 10 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: {
